52a2f93836dd0b4e15eba3e6e6ed7f08b5ea97a7
Nam Woo <pre3ice@gmail.com>
2023-01-18 13:04:53 +0900

    jetson 에서 syntax error 왜?

--- scripts/pub2.py
+++ scripts/pub2.py
@@ -1,39 +1,27 @@
-
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys, time
 
+##from https://github.com/NamWoo
+
+from __future__ import print_function
+#import roslib
+#roslib.load_manifest('my_package')
+import sys, time
+import rospy
 import cv2
 import numpy as np
-
-import rospy
-
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-pipeline_1 = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
-
-pipeline_2 = " ! ".join(["v4l2src device=/dev/video0",
-                       "video/x-raw, width=640, height=480, framerate=15/1",
-                       "videoconvert",
-                       "video/x-raw, format=(string)BGR",
-                       "appsink"
-                       ])
-
-pipeline_h264 = " ! ".join(["v4l2src device=/dev/video0",
-                            "video/x-h264, width=1280, height=720, framerate=30/1, format=H264",
-                            "avdec_h264",
-                            "videoconvert",
-                            "video/x-raw, format=(string)BGR",
-                            "appsink sync=false"
-                            ])
-
-
 
 def show_camera():
-    
+    pipeline_1 = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 
-    
+    #pipeline_2 = "v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480, framerate=15/1 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+
+    #pipeline_3 = "v4l2src device=/dev/video0 ! video/x-h264, width=1280, height=720, framerate=30/1, format=H264 ! avdec_h264 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink sync=false"
+
+        
     cap = cv2.VideoCapture(pipeline_1, cv2.CAP_GSTREAMER)
     width = 640
     height = 480
@@ -57,7 +45,6 @@ def show_camera():
     
     if cap.isOpened():
         try:
-
             while True:
                 ret_val, frame = cap.read()
                 rgb_pub.publish(bridge.cv2_to_imgmsg(frame, 'bgr8'))
