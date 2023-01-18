ec7d1372b574c77630a5d1e2ea3082b1712b38a2
Nam Woo <pre3ice@gmail.com>
2023-01-18 12:32:22 +0900

    fixing ros img pub

---
+++ scripts/pub2.py
@@ -0,0 +1,83 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys, time
+
+import cv2
+import numpy as np
+
+import rospy
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+pipeline_1 = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)15/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+
+pipeline_2 = " ! ".join(["v4l2src device=/dev/video0",
+                       "video/x-raw, width=640, height=480, framerate=15/1",
+                       "videoconvert",
+                       "video/x-raw, format=(string)BGR",
+                       "appsink"
+                       ])
+
+pipeline_h264 = " ! ".join(["v4l2src device=/dev/video0",
+                            "video/x-h264, width=1280, height=720, framerate=30/1, format=H264",
+                            "avdec_h264",
+                            "videoconvert",
+                            "video/x-raw, format=(string)BGR",
+                            "appsink sync=false"
+                            ])
+
+
+
+def show_camera():
+    
+
+    
+    cap = cv2.VideoCapture(pipeline_1, cv2.CAP_GSTREAMER)
+    width = 640
+    height = 480
+    fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    now = time.time()
+    save_name = time.strftime('%m%d_%H%M%S', time.localtime(now))
+    savepath = '/root/data/temp/{}.avi'.format(save_name)
+    
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print('get :', frame_width, frame_height, width, height)
+    
+    # out = cv2.VideoWriter(savepath, fourcc, fps, (width, height), True)
+    
+    
+    rospy.init_node('rgb_pub', anonymous=True)
+    rgb_pub = rospy.Publish('rgb', Image, queue_size=1)
+    bridge = CvBridge()
+    
+    
+    if cap.isOpened():
+        try:
+
+            while True:
+                ret_val, frame = cap.read()
+                rgb_pub.publish(bridge.cv2_to_imgmsg(frame, 'bgr8'))
+                # out.write(frame)
+
+                # cv2.imshow('frame', frame)
+                # keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                # if keyCode == 27 or keyCode == ord('q'):
+                #     break
+
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()
+            print('end')
+    else:
+        print("Error: Unable to open camera")
+
+
+if __name__ == '__main__':
+
+    show_camera()
+    
